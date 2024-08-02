4add21e0bc40c791e424726199fa010a60e30883
rukianna <ruk.sakmak@gmail.com>
2024-08-03 01:00:07 +0300

    Update mgr.js

--- docs/studies/datapipe.example.iat1/mgr.js
+++ docs/studies/datapipe.example.iat1/mgr.js
@@ -9,7 +9,7 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
+	init_data_pipe(API, '6bSEw0cY17ebU5FwejY4jXQfSaQ3esgY9jIQvcEYucCJCicHQCqy4Yij8LtFv2ixUJN4HZ',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
